3cfc5ca1613dc714ca12d2e5c214761ef8e69744
Signal-flow12 <tcravenny@gmail.com>
2023-05-04 11:43:14 -0400

    Finshed fundemental labs

--- labs_examples/fundamentals/labs/Exercise_04.java
+++ labs_examples/fundamentals/labs/Exercise_04.java
@@ -11,9 +11,23 @@ package labs_examples.fundamentals.labs;
 public class Exercise_04 {
 
     public static void main(String[] args) {
+        byte myByte = 10;
+        System.out.println(myByte);
+        short myShort = 32000;
+        System.out.println(myShort);
+        int myInt = 1;
+        System.out.println(myInt);
+        long myLong = 100_000_000;
+        System.out.println(myLong);
+        float decimal = 3.24f;
+        System.out.println(decimal);
+        double myDouble = 4.239392;
+        System.out.println(myDouble);
+        boolean isTrue = true;
+        System.out.println(isTrue);
+        char myChar = 'T';
+        System.out.println(myChar);
 
-        int i = 1;
-        System.out.println("int i is: " + i);
 
         // write your code below
 
--- labs_examples/fundamentals/labs/Exercise_05.java
+++ labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,15 +14,23 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int strLength = str.length();
+        System.out.println(strLength);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean compareStrings = str == str2;
+
+        System.out.println(compareStrings);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String combineStrings = str + str2;
+        System.out.println(combineStrings);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
-
+        String indexOfstr = String.valueOf(str.indexOf('o'));
+        System.out.println(indexOfstr);
     }
 
 
